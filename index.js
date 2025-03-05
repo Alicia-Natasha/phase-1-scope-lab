13377ae99a9fa05f4cfb7446a632173ef5db6dca
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-05 21:48:04 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+{
+    console.log(`Global Scope type ${customerName}`);
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(`Global Scope type ${customerName}`);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+    newBestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bubble bass';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bubble bass';
+}
